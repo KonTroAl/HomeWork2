1d3baee9d6bdbf39af5bab70fa0f77d520611d21
KonTroAl <kostya98295@mail.ru>
2020-05-15 23:46:24 +0500

    Task 4 copmplit

--- 4.py
+++ 4.py
@@ -1,3 +1,8 @@
 n = input("Введите любую фразу: ").split()
+for i in range(len(n)):
+    if len(list(n[i])) > 10:
+        m = list(n[i])
+        n[i] = "".join(m[:10])
+
 for k, i in enumerate(n):
     print(k + 1, i)
