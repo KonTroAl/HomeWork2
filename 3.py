db4b9e8073eb73bbe9f5ba2e1670d3e9c91643d3
KonTroAl <kostya98295@mail.ru>
2020-05-15 23:06:36 +0500

    Task 3 version 2 complit

--- 3.py
+++ 3.py
@@ -1,5 +1,6 @@
+# version 1
 n = int(input("Введите порядковый номер месяца: "))
-month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 b = n - 1
 
 if month[b] == 12:
@@ -12,4 +13,13 @@ elif 6 <= month[b] <= 8:
     print("summer")
 elif 9 <= month[b] <= 11:
     print("autumn")
-#############
+
+# version 2
+n = int(input("Введите порядковый номер месяца: "))
+month = {1: "winter", 2: "winter",
+         3: "spring", 4: "spring", 5: "spring",
+         6: "summer", 7: "summer", 8: "summer",
+         9: "autumn", 10: "autumn", 11: "autumn",
+         12: "winter"
+         }
+print(month.get(n))
