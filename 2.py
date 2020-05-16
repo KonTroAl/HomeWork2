045581ebf43ee07b91d64de6e19f745fe073e1d9
KonTroAl <kostya98295@mail.ru>
2020-05-16 22:37:41 +0500

    Task 2
    Практически полностью удалось соблюсти условия второй задачи за исключением обмена между 2 и 3 значениями

--- 2.py
+++ 2.py
@@ -1,5 +1,19 @@
-my_list = [1, 2, 3, 4, 5]
+lenght = int(input("Какой длины будет ваш список? "))
+i = 0
+my_list = []
+while i < lenght:
+    n = int(input("Enter you number: "))
+    my_list.append(n)
+    i = i + 1
+print(my_list)
+
 new_list = []
-for i in range(len(my_list)):
-    new_list.insert(i, my_list[i])
+for el in range(len(my_list) - 1):
+    var_1 = my_list[el]
+    var_2 = my_list[el + 1]
+    var_1, var_2 = var_2, var_1
+    new_list.append(var_1)
+    new_list.append(var_2)
+new_list.pop()
+
 print(new_list)
