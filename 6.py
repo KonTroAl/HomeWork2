5d9521f888853c48255b73953a83aa69bf381a63
KonTroAl <kostya98295@mail.ru>
2020-05-16 23:20:45 +0500

    Half of Task 6 complit

--- 6.py
+++ 6.py
@@ -1,1 +1,22 @@
-name 
+# name = input("Введите наименование товаров через пробел: ").split()
+# price = input("Введите цену каждого товара через пробел: ").split()
+# value = input("Введите количество каждого товара через пробел: ").split()
+
+# print(name, price, value)
+
+number = int(input("Enter number of values: "))
+name = ["компьютер", "принтер", "сканер"]
+price = [20000, 6000, 2000]
+value = [5, 2, 7]
+i = 0
+st = []
+
+while i < number:
+    n = name[i]
+    p = price[i]
+    v = value[i]
+    list = {"название": n, "цена": p, "количество": v, "ед": "шт."}
+    i = i + 1
+    cort = (i, list)
+    st.insert(i, cort)
+print(st)
