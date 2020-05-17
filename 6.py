3c6d5a1ffa4b7a254d58c12ce32c073ff945cd36
KonTroAl <kostya98295@mail.ru>
2020-05-17 23:12:38 +0500

    Complit Task 6

--- 6.py
+++ 6.py
@@ -2,22 +2,24 @@ name = input("Введите наименование товаров через 
 price = input("Введите цену каждого товара через пробел: ").split()
 value = input("Введите количество каждого товара через пробел: ").split()
 
-# print(name, price, value)
-
 number = len(name)
-#name = ["компьютер", "принтер", "сканер"]
-#price = [20000, 6000, 2000]
-#value = [5, 2, 7]
 i = 0
 st = []
 
-while i < number:
+for i in range(number):
     n = name[i]
     p = price[i]
     v = value[i]
-    list = {"название": n, "цена": p, "количество": v, "ед": "шт."}
+    list = {"название ": n, "цена ": p, "количество ": v, "ед ": "шт."}
     i = i + 1
     cort = (i, list)
     st.insert(i, cort)
 print(st)
 
+sum = {
+    "название": name,
+    "цена": price,
+    "количество": value,
+    "ед ": "шт."
+}
+print(sum)
